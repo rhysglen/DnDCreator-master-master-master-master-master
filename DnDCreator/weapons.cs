4b9f16da04886f13442d70bbec2063bd97629024
temp <temo>
2018-06-15 14:31:28 -0400

    Add project files.

---
+++ DnDCreator/weapons.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnDCreator
+{
+    public class weapons
+    {
+        public string name, damage;
+        public int atkBonus;
+
+        public weapons (string _name, int _atkBonus, string _damage )
+        {
+            name = _name;
+            atkBonus = _atkBonus;
+            damage = _damage;
+        }
+    }
+}
